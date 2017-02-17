c61e4272d4c6b13b2a31bfbaa438dcefd7d8ef03
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-02-17 10:01:11 +0800

    optimized backend frontend js separation

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,20 +1,19 @@
 
-var apiKey = "39af7508c220333711bac0fc738b2d43";
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
+
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
-    // function(response) {
-    //       console.log(response);
-    //       console.log(JSON.stringify(response));
-    // $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    //       console.log("The humidity in " + city + " is " + response.main.humidity + "%");
-    //  });
-    console.log("Notice: The GET request has been made.");
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
 });
+
+
+//       console.log(JSON.stringify(response));
